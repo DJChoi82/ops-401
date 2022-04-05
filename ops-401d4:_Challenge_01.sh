d5bce4ff125c9d6ad291907ce0027dbcf0880ad6
DJ Choi <95465339+DJChoi82@users.noreply.github.com>
2022-04-05 14:52:35 -0700

    Create ops-401d4:_Challenge_01.sh

---
+++ ops-401d4:_Challenge_01.sh
@@ -0,0 +1,16 @@
+#!/bin/python3
+#Class 2 Python Script
+#DJ Choi
+#4/5/2022
+#Writing pythong script to evaluate hosts on Lan with ICMP packets
+
+import datetime
+
+import time
+
+import os
+
+now = datetime.datetime.now()
+
+ip = "127.0.0.1"
+
