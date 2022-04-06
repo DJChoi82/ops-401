692c6d2a2f3f0107639ff472098c4056d9fdbd0f
DJ Choi <djchoi82@gmail.com>
2022-04-05 17:49:45 -0700

    push

--- challenge1.py
+++ challenge1.py
@@ -4,40 +4,36 @@
 #4/5/2022
 #Writing pythong script to evaluate hosts on Lan with ICMP packets
 
-#importing pythonping
-from pythonping import ping
-
 #importing datetime library
 import datetime
 
 #importing time library
 import time
 
+#import OS library
+import os
+
 #assigning variable now to date and time
 now = datetime.datetime.now()
 
 #assigning variable ip to an ip address
-ip = input("Enter IP address:")
+ip = "127.0.0.1"
 
 #creating an infinite loop
 while True:
     #pinging IP
-    response_list = ping(ip, count=1,)
+    response = (os.system("ping -c 1 " + ip))
     #checking if ping is successful
-    if response_list.success(option=3):
+    if response == 0:
        status = "Network Active to"
     else:
        status = "Network Inactive to"
-
+    #print space
+    print()
     #print date, time, status, and ip
     print(str(now), status, ip)
     #print space
     print()
     #sleep for 2 seconds
     time.sleep(2)
-
-
-
-
-
 
