6b5f0d52e9c535d53e4197d36903454e9ffe333f
DJ Choi <djchoi82@gmail.com>
2022-04-13 00:28:50 -0700

    push

--- ops-401d4:_Challenge_07.py
+++ ops-401d4:_Challenge_07.py
@@ -4,16 +4,26 @@
 #4/12/2022
 #Writing python script to encrypt and decrypt a message and file. 
 
-
-
-# Import Libraries
+#import libraries
 from cryptography.fernet import Fernet
 import os
+import os.path
 
-# Declare Functions
+#declare functions
+
+#generates key and save it to a file
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+#load key
+def load_key():
+    return open("key.key", "rb").read()
+
 #menu
 def menu():
-    choice = input("Options:\n[1] Encrypt a file\n[2] Decrypt a file\n[3] Encrypt a message\n[4] Decrypt a message\n[5] Encrypt a folder and all its contents\n[6] Decrypt a folder and all its contents\nEnter a number:")
+    choice = input("Options:\n[1] Encrypt a file\n[2] Decrypt a file\n[3] Encrypt a message\n[4] Decrypt a message\n[5] Encrypt a folder and all its contents\n[6] Decrypt a folder and all its contents\nEnter a number: ")
 
     if choice == '1':  
         filename = input("Enter filepath: ")
@@ -33,44 +43,39 @@ def menu():
 
     elif choice == '5':
         folder = input("Enter foler path: ")
-        directory(folder)
+        encrypt_dir(folder)
 
     elif choice == '6':
         folder = input("Enter folder path: ")
-        directory(folder)
+        decrypt_dir(folder)
     else:
         print("Invalid choice. Please try again.")
 
-#generates key and save it to a file
-def write_key():
-    key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)
-
-#load key
-def load_key():
-    return open("key.key", "rb").read()
+#checking if key file exist
+def key_check():
+    if os.path.isfile("./key.key"):
+        key = load_key()
+    else:
+        write_key()
+        key = load_key()
+    global f
+    f = Fernet(key)
 
 #encrypt a message
 def encrypt_message(message):
-    write_key()
-    key = load_key()
-    f = Fernet(key)
+    key_check()
     encrypted = f.encrypt(message)
     print("Ciphertext is "+encrypted.decode('utf-8'))
 
 #decrypt a message
 def decrypt_message(message):
-    key = load_key()
-    f = Fernet(key)
+    key_check()
     decrypted = f.decrypt(message)
     print("Plaintext is "+str(decrypted.decode('utf-8')))
 
 #encrypt file
 def encrypt(filename):
-    write_key()
-    key = load_key()
-    f = Fernet(key)
+    key_check()
     with open(filename, "rb") as file:
         file_data = file.read()
     encrypted_data = f.encrypt(file_data)
@@ -80,8 +85,7 @@ def encrypt(filename):
 
 #decrypt file
 def decrypt(filename):
-    key = load_key()
-    f = Fernet(key)
+    key_check()
     with open(filename, "rb") as file:
         encrypted_data = file.read()
     decrypted_data = f.decrypt(encrypted_data)
@@ -89,13 +93,29 @@ def decrypt(filename):
         file.write(decrypted_data)
     print("File Decrypted")
 
-# Begin recursive directory crawl
-def directory(folder):
+#encrypt directory and contents
+def encrypt_dir(folder):
     for root, dirs, files in os.walk(folder, topdown=False):
         for name in files:
-            print(os.path.join(root, name))
+            file_path = (os.path.join(root, name))
+            print(file_path)
+            encrypt(file_path)
         for name in dirs:
-            print(os.path.join(root, name))
+            file_path = (os.path.join(root, name))
+            print(file_path)
+            encrypt(file_path)
+
+#decrypt directory and contents
+def decrypt_dir(folder):
+    for root, dirs, files in os.walk(folder, topdown=False):
+        for name in files:
+            file_path = (os.path.join(root, name))
+            print(file_path)
+            decrypt(file_path)
+        for name in dirs:
+            file_path = (os.path.join(root, name))
+            print(file_path)
+            decrypt(file_path)
 
 # Main
 
