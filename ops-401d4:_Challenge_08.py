0e09ca500699ba287164f4a1e3a98c35087f1b5f
DJ Choi <djchoi82@gmail.com>
2022-04-13 15:27:02 -0700

    push

---
+++ ops-401d4:_Challenge_08.py
@@ -0,0 +1,136 @@
+#!/bin/python3
+#Class 8 Python Script
+#DJ Choi
+#4/13/2022
+#Writing python script to encrypt and decrypt a message and file. 
+
+#import libraries
+from cryptography.fernet import Fernet
+import os
+import os.path
+import ctypes
+
+#declare functions
+
+#generates key and save it to a file
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+#load key
+def load_key():
+    return open("key.key", "rb").read()
+
+#menu
+def menu():
+    choice = input("Options:\n[1] Encrypt a file\n[2] Decrypt a file\n[3] Encrypt a message\n[4] Decrypt a message\n[5] Encrypt a folder and all its contents\n[6] Decrypt a folder and all its contents\n[7] Ransomware simulation\n[8] Reverse ransomware simulation\nEnter a number: ")
+
+    if choice == '1':  
+        filename = input("Enter filepath: ")
+        encrypt_file(filename)
+
+    elif choice == '2':
+        filename = input("Enter filepath: ")
+        decrypt_file(filename)
+
+    elif choice == '3':
+        message = input("Enter message: ")
+        encrypt_message(message.encode())
+
+    elif choice == '4':
+        message = input("Enter message: ")
+        decrypt_message(message.encode())
+
+    elif choice == '5':
+        folder = input("Enter foler path: ")
+        encrypt_dir(folder)
+
+    elif choice == '6':
+        folder = input("Enter folder path: ")
+        decrypt_dir(folder)
+
+    elif choice == '7':
+        MessageBox = ctypes.windll.user32.MessageBoxW  # MessageBoxA in Python2
+        MessageBox(None, 'Message\nfor\nthe\nuser', 'Window title', 0)
+
+    elif choice == '8':
+        folder = input("Enter folder path: ")
+        
+
+    else:
+        print("Invalid choice. Please try again.")
+
+#writing key if it doesn't exist and load key
+def key_check():
+    if os.path.isfile("./key.key"):
+        key = load_key()
+    else:
+        write_key()
+        key = load_key()
+    global f
+    f = Fernet(key)
+
+#encrypt a message
+def encrypt_message(message):
+    key_check()
+    encrypted = f.encrypt(message)
+    print("Ciphertext is "+encrypted.decode('utf-8'))
+
+#decrypt a message
+def decrypt_message(message):
+    key_check()
+    decrypted = f.decrypt(message)
+    print("Plaintext is "+str(decrypted.decode('utf-8')))
+
+#encrypt file
+def encrypt_file(filename):
+    key_check()
+    with open(filename, "rb") as file:
+        file_data = file.read()
+    encrypted_data = f.encrypt(file_data)
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
+    print("File encrypted")
+
+#decrypt file
+def decrypt_file(filename):
+    key_check()
+    with open(filename, "rb") as file:
+        encrypted_data = file.read()
+    decrypted_data = f.decrypt(encrypted_data)
+    with open(filename, "wb") as file:
+        file.write(decrypted_data)
+    print("File Decrypted")
+
+#encrypt directory and contents
+def encrypt_dir(folder):
+    for root, dirs, files in os.walk(folder, topdown=False):
+        for name in files:
+            file_path = (os.path.join(root, name))
+            print(file_path)
+            encrypt_file(file_path)
+        for name in dirs:
+            file_path = (os.path.join(root, name))
+            print(file_path)
+            encrypt_file(file_path)
+
+#decrypt directory and contents
+def decrypt_dir(folder):
+    for root, dirs, files in os.walk(folder, topdown=False):
+        for name in files:
+            file_path = (os.path.join(root, name))
+            print(file_path)
+            decrypt_file(file_path)
+        for name in dirs:
+            file_path = (os.path.join(root, name))
+            print(file_path)
+            decrypt_file(file_path)
+
+# Main
+
+menu()
+
+#end
+
+
