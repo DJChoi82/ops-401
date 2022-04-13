92957072ac347ac483a34d0d78a34d7462c94d7c
DJ Choi <djchoi82@gmail.com>
2022-04-12 17:21:25 -0700

    push

--- ops-401d4:_Challenge_06.py
+++ ops-401d4:_Challenge_06.py
@@ -11,71 +11,60 @@ from cryptography.fernet import Fernet
 
 # Declare Functions
 
+#generates key and save it to a file
 def write_key():
-    # Generates a key and save it into a file
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
 
+#load key
 def load_key():
-    # Loads the key from the current directory named `key.key`
     return open("key.key", "rb").read()
 
-def encrypt(filename, key):
-    #Given a filename (str) and key (bytes), it encrypts the file and write it
+#encrypt a message
+def encrypt_message(message):
+    write_key()
+    key = load_key()
+    f = Fernet(key)
+    encrypted = f.encrypt(message)
+    print("Ciphertext is "+encrypted.decode('utf-8'))
+
+#decrypt a message
+def decrypt_message(message):
+    key = load_key()
+    f = Fernet(key)
+    decrypted = f.decrypt(message)
+    print("Plaintext is "+str(decrypted.decode('utf-8')))
+
+#encrypt file
+def encrypt(filename):
+    write_key()
+    key = load_key()
     f = Fernet(key)
     with open(filename, "rb") as file:
-        # read all file data
         file_data = file.read()
-    # encrypt data
     encrypted_data = f.encrypt(file_data)
-    # write the encrypted file
     with open(filename, "wb") as file:
         file.write(encrypted_data)
+    print("File encrypted")
 
-def decrypt2(filename, key):
-    #Given a filename (str) and key (bytes), it decrypts the file and write it
+#decrypt file
+def decrypt(filename):
+    key = load_key()
     f = Fernet(key)
     with open(filename, "rb") as file:
-        # read the encrypted data
         encrypted_data = file.read()
-    # decrypt data
     decrypted_data = f.decrypt(encrypted_data)
-    # write the original file
     with open(filename, "wb") as file:
         file.write(decrypted_data)
+    print("File Decrypted")
+
 # Main
 
-ip  = input("enter")
 
-# Generate and write a new key
-write_key()
 
-# load the previously generated key
-key = load_key()
-#print("Key is "+str(key.decode('utf-8')))
 
-message = ip.encode()
-print("Plaintext is "+str(message.decode('utf-8')))
 
-# Initialize the Fernet class
-f = Fernet(key)
 
-# Encrypt the message
-encrypted = f.encrypt(message)
 
-# Print how it looks
-print("Ciphertext is "+encrypted.decode('utf-8'))
 
-print()
-# Decrypt the message
-#decrypted = f.decrypt(ip)
-#print(decrypted)
-
-#file name
-#file = "data.csv"
-#encrypt
-#encrypt(file, key)
-
-#decrypt
-#decrypt(file, key)
